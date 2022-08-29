a55409330343f7f76d9f6147689ecd30d6befd8c
erictelkkala <eric.telkkala@gmail.com>
2022-08-29 15:27:40 +0300

    Task 8.8

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -1,30 +1,57 @@
-const Authors = (props) => {
-  if (!props.show) {
-    return null
-  }
-  const authors = []
+import { gql, useQuery } from "@apollo/client"
+import React from "react"
 
-  return (
-    <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>born</th>
-            <th>books</th>
-          </tr>
-          {authors.map((a) => (
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+const Authors = (props) => {
+    const GET_AUTHORS = gql`
+        query {
+            allAuthors {
+                name
+                born
+                bookCountByAuthor
+            }
+        }
+    `
+
+    // Get the states of the query and assign them to variables
+    const { loading, error, data } = useQuery(GET_AUTHORS)
+
+    // Do not show the page if it is not active
+    // eslint-disable-next-line react/prop-types
+    if (props.show !== true) {
+        return null
+    }
+
+    // Loading and error messages
+    if (loading) {
+        return <div>loading...</div>
+    } else if (error) {
+        return <div>error</div>
+
+        // Show the table if there are authors
+    } else {
+        console.log("authors", data)
+        return (
+            <div>
+                <h2>authors</h2>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th></th>
+                            <th>Born</th>
+                            <th>Books</th>
+                        </tr>
+                        {data.allAuthors.map((author) => (
+                            <tr key={author.name}>
+                                <td>{author.name}</td>
+                                <td>{author.born}</td>
+                                <td>{author.bookCountByAuthor}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 }
 
 export default Authors
--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App"
+import {
+    ApolloProvider,
+    ApolloClient,
+    InMemoryCache,
+    HttpLink,
+} from "@apollo/client"
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+        uri: "http://localhost:4000",
+    }),
+})
+
+const container = document.getElementById("root")
+const root = createRoot(container)
+
+root.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+)
